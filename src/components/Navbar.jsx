2ecf10a65879628c227d370a3ab403275254c4a1
ankita ray <ankita.ray@sendfast.in>
2024-04-30 18:03:44 +0530

    drawer padding changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = ({
     <>
       <div className="flex bg-white text-teal-600 items-center justify-between w-full px-10 py-2 z-50 shadow-md top-0 fixed md:hidden">
         <a href="/">
-          <img src={logo} alt="Logo" className="w-14" />
+          <img src={logo} alt="Logo" className="w-14 mt-2" />
         </a>
         <Hamburger toggled={isOpen} toggle={setOpen} />
       </div>
       <nav className={`hidden md:flex font-roboto-slab text-xl bg-white text-teal-600 items-center justify-between w-full px-10 py-2 z-50 shadow-md top-0 fixed transition-transform duration-500 ${
-        isOpen ? "translate-y-14" : "translate-y-0"
+        isOpen ? "translate-y-14 transition duration-300" : "translate-y-0"
       }`}>
         <ul className="flex items-center justify-center gap-10 cursor-pointer">
           <a href="/">
@@ -46,13 +46,10 @@ const Navbar = ({
       <div
         className={`${
           isOpen ? "block" : "hidden"
-        } md:hidden bg-white text-teal-600 fixed top-16 left-0 w-full z-50 shadow-md transition-max-height duration-500 overflow-hidden`}
-        style={{
-          maxHeight: isOpen ? "500px" : "0",
-          transitionDuration: isOpen ? "500ms" : "200ms" // Adjust the duration as needed
-        }}
+        } transition-max-height duration-500 overflow-hidden md:hidden bg-white text-teal-600 fixed top-16 left-0 w-full z-50 shadow-md `}
+        
       >
-        <ul className="ml-12 my-6">
+        <ul className="ml-12 my-2">
           <li className="py-2" onClick={() => { aboutScroll(); setOpen(false); }}>About</li>
           <li className="py-2" onClick={() => { skillsScroll(); setOpen(false); }}>Skills</li>
           <li className="py-2" onClick={() => { experienceScroll(); setOpen(false); }}>Experience</li>
